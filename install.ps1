ac87f1be4edd3a4ee80e1e8e9a3d534138422b93
shxcry <xintapayyasiriphumi@gmail.com>
2026-04-27 21:25:03 +0700

    fix locate

--- install.ps1
+++ install.ps1
@@ -8,7 +8,7 @@ $KEYAUTH_OWNERID = "h73NBoWgLW"
 $KEYAUTH_VERSION = "1.0"
 $KEYAUTH_URL     = "https://keyauth.win/api/1.2/"
 
-$EXE_URL  = "https://github.com/xintapayyasiriphumi-web/reshadexinstaller/releases/download/v1.0.1/ReShadeInstaller.exe"
+$EXE_URL  = "https://github.com/xintapayyasiriphumi-web/reshadexinstaller/releases/download/v1.0.2/ReShadeInstaller.exe"
 $EXE_NAME = "ReShadeInstaller.exe"
 $EXE_PATH = "$env:TEMP\$EXE_NAME"
 
